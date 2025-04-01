e25039a53176a3fa7483e40a085d5052cf794363
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-01 10:21:04 -0300

    fix: Altera rota

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -102,7 +102,7 @@ export default function NavbarSocial({ LightMode }) {
             offset={-100}
             href="#"
           >
-             <img
+            <img
               src={content.texts.navbar.logo.img}
               alt={content.texts.navbar.logo.alt}
               className={`${
@@ -125,7 +125,8 @@ export default function NavbarSocial({ LightMode }) {
               <Button
                 aria-label={content.texts.hero.ctaButtonAriaLabel}
                 label={content.texts.navbar.ctaButtonTextResponsive}
-                onClick={() => navigate("/whatsapp")}
+                // onClick={() => navigate("/whatsapp")}
+                buttonLink={content.texts.links.ctaWhatsapp}
                 className={`${scrolling ? "" : ""}`}
                 size="small"
                 icon={<FaWhatsapp />}
