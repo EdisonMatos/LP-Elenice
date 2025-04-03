6edd5732e1a898497caf8c815e87f7e3271245a5
Edison Matos <edison.matos@live.com>
2025-04-03 15:19:47 -0300

    feat: Atualiza cor primária

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
   extend: {
     colors: {
-      primary: "#FFB854",
+      primary: "#F2E397",
       secondary: "#000000",
       bgSectionLight: "#FFFFFF",
       bgSectionDark: "#182337",
